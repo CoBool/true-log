29d2aad62249cdbd981d07ca53226034382c851f
배상빈 <saokm5@gmail.com>
2026-04-30 16:59:47 +0900

    feat: add sidebar shell foundation

---
+++ src/lib/components/shell/SidebarFooter.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { site } from '$lib/config/site';
+</script>
+
+<footer class="mt-auto hidden border-t border-gray-800 pt-5 text-sm text-gray-500 sm:block">
+	<p class="m-0">{site.name}</p>
+</footer>
